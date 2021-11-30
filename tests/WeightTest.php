a68b25ca4e8eac36ca6b97d52ef7b5eb31e5b729
ruslanashaari <ruslan@conversant.com.my>
2021-11-30 17:12:26 +0800

    improve test name

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -2,7 +2,7 @@
 
 use Lanash\WeightConversion\WeightConversion;
 
-it('can convert kilograms to pound', function () {
+it('can convert kilograms to pound correctly', function () {
     $pounds = WeightConversion::kilograms(100)->toPounds();
 
     expect($pounds)->toEqual(220.46);
